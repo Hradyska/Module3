a9f485cd3a97e48cf8118ccf9e8585f31ee88ba8
Larysa Hradyska <l.v.hradyska@gmail.com>
2023-03-22 20:15:51 +0200

    adding solution

---
+++ Module3/Module3/Program.cs
@@ -0,0 +1,25 @@
+﻿using static Module3.SomeAsyncMethods;
+namespace Module3
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            List<int> list1 = new List<int>();
+            Random rand = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                list1.Add(rand.Next(0, 100));
+            }
+
+            Run += MethodOne;
+            Run += MethodTwo;
+            Run += MethodThree;
+            var res = await OnRun(list1);
+            foreach (var item in res)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Module3/Module3/SomeAsyncMethods.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module3
+{
+    internal class SomeAsyncMethods
+    {
+        public static event Func<List<int>, Task<List<int>>> Run;
+        public static async Task<List<int>> MethodOne(List<int> list)
+        {
+            Task<List<int>> task = Task.FromResult(list.Distinct().ToList());
+            await task;
+            var distinctList = task.Result;
+            _ = Task.Delay(3000);
+            return distinctList;
+        }
+
+        public static async Task<List<int>> MethodTwo(List<int> list)
+        {
+            Task<List<int>> task = Task.FromResult(list.Take(3).ToList());
+
+            await task;
+            var taketList = task.Result;
+            _ = Task.Delay(3000);
+            return taketList;
+        }
+
+        public static async Task<List<int>> MethodThree(List<int> list)
+        {
+            Task<List<int>> task = Task.FromResult(list.Where(x => x % 2 == 0).Where(x => x > 10).Order().ToList());
+            await task;
+            var taketList = task.Result;
+            _ = Task.Delay(3000);
+            return taketList;
+        }
+
+        public static async Task<List<int>> OnRun(List<int> list)
+        {
+            Func<List<int>, Task<List<int>>> handler = Run;
+
+            if (handler == null)
+            {
+                return null;
+            }
+
+            Delegate[] invocationList = handler.GetInvocationList();
+            List<int> result = new ();
+            foreach (Delegate @delegate in invocationList)
+            {
+                Task<List<int>> task = (Task<List<int>>)@delegate.DynamicInvoke(list);
+                await task;
+                result = result.Concat(task.Result).ToList();
+            }
+
+            return result;
+        }
+    }
+}
